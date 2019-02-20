1cb2d65cb3aa64bc92c17e0bc0aed18a9d70be5a
wangzhangjia <736012593@qq.com>
2019-02-20 15:26:00 +0800

    --all

---
+++ src/main/java/com/example/wzjconfig/WzjConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.wzjconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzjConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WzjConfigApplication.class, args);
+	}
+
+}
